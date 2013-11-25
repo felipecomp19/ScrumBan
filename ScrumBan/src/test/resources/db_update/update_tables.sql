49f4d1098d5ee121b98c071f4ed7652981baafa1
Felipe Teixeira <felipecomp19@gmail.com>
2013-11-24 22:20:03 -0200

    chaged db configs again and corrected de update_tables script

--- ScrumBan/src/test/resources/db_update/update_tables.sql
+++ ScrumBan/src/test/resources/db_update/update_tables.sql
@@ -1,3 +1,7 @@
-INSERT INTO db_scrumban_test.tb_status (txt_name) VALUES ("To do");
+INSERT INTO db_scrumBan_test.tb_user (txt_email,txt_name,txt_password) VALUES ("felipe@gmail.com", "Felipe", "123456");
+INSERT INTO db_scrumBan_test.tb_project (dt_createdDate,txt_description,txt_title,createdByUser_id) VALUES ("2013-07-08","Just a hit","ScrumBan",1);
+INSERT INTO db_scrumBan_test.tb_project_member (project_id, user_id) VALUES ('1', '1');
+
+INSERT INTO db_scrumBan_test.tb_status (txt_name) VALUES ("To do");
 INSERT INTO db_scrumBan_test.tb_status (txt_name) VALUES ("Doing");
 INSERT INTO db_scrumBan_test.tb_status (txt_name) VALUES ("Done");
